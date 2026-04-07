bd25ac92d162f0df813b732a6b33cf1886ef133b
Vishal <saivishalv4u@gmail.com>
2026-04-07 12:42:25 +0530

    Add files via upload
    DINCAE Model

---
+++ run_DINCAE.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import DINCAE
+
+
+#filename = "/path/to/file.nc"
+#varname = "SST"
+#outdir = "/path/to/output/dir"
+
+filename = r"E:\New_data_DINCAE\IR_DINCAE_ready_wind.nc"
+varname = "wind_speed"
+outdir = r"E:\dincaep\Output"
+
+
+DINCAE.reconstruct_gridded_nc(
+	filename,
+	varname,
+	outdir,
+	epochs=400,
+	batch_size=30,
+	learning_rate=2e-5,
+	learning_rate_decay_epoch=80,
+	dropout_rate_train=0.08,
+	jitter_std=0.01,
+	nepoch_keep_missing=20,
+	save_each=25,
+	save_model_each=25,
+	iseed=42,
+)
